32ecfa278bcdbcfd88832a95de0deef23ce70919
javapotato24 <evan.wu25@ycdsbk12.ca>
2024-09-13 09:52:39 -0400

    added code review 3

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -11,7 +11,7 @@ public class Review2{
         System.out.print("0 - print a joke about your hair" + '\n' + "1 - print a joke about your feet" + '\n' + "2 - print a joke about your clothes" + '\n' +"3 - print a joke about your teacher" + '\n');
         
         System.out.print("choose a menu option: ");
-        intMenu = Integer.parseInt(menu.readLine());
+        intMenu = Integer.parseInt(menu.readLine());    
 
     }
 }
--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -1,1 +1,25 @@
 package gr11review.part1;
+
+import java.io.*;
+
+public class Review3{
+    public static void main(String[] args) throws IOException{
+         int intCount = 21;
+            
+         while(intCount <= 100){ 
+            if(intCount % 2 > 0){
+                System.out.println(intCount);
+            }
+            intCount += 1;
+         }
+
+         System.out.print(" \n");
+
+         intCount = 29; 
+         while(intCount >= 2){ 
+            System.out.println(intCount);
+            intCount -= 1;
+         }
+
+    }
+}

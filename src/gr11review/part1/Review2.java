89989667703d98dd2e47e62346c2d2009c56060d
javapotato24 <evan.wu25@ycdsbk12.ca>
2024-09-13 10:35:46 -0400

    finished review 2

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -8,10 +8,31 @@ public class Review2{
 
         int intMenu;
 
-        System.out.print("0 - print a joke about your hair" + '\n' + "1 - print a joke about your feet" + '\n' + "2 - print a joke about your clothes" + '\n' +"3 - print a joke about your teacher" + '\n');
+        // System.out.print("0 - print a joke about your hair" + '\n' + "1 - print a joke about your feet" + '\n' + "2 - print a joke about your clothes" + '\n' +"3 - print a joke about your teacher" + '\n');
+        System.out.println("0 - print a joke about your hair");
+        System.out.println("1 - print a joke about your feet");
+        System.out.println("2 - print a joke about your clothes");
+        System.out.println("3 - print a joke about your teacher");
         
-        System.out.print("choose a menu option: ");
-        intMenu = Integer.parseInt(menu.readLine());    
+        System.out.print("Choose a menu option: ");
+        intMenu = Integer.parseInt(menu.readLine());
 
+        switch(intMenu){
+            case 0:
+                System.out.println("Why do bees have sticky hair? \nThey always use honeycombs. ");
+                break;
+            case 1:
+                System.out.println("What do frogs wear on their feet? \nOpen-toed sandals.");
+                break;
+            case 2:
+                System.out.println("What do old sheep use to keep their hands warm? \nMuttons.");
+                break;
+            case 3:
+                System.out.println("Why was the teacher wearing sunglasses in the classroom? \nBecause the class was so bright.");
+                break;
+            default:
+                System.out.println("Invalid menu option");
+                break;
+        }
     }
 }
--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,2 +1,11 @@
 package gr11review.part1;
 
+import java.io.*;
+import java.text.DecimalFormat.*;
+
+public class Review4{
+    public static void main(String[] args) throws IOException{
+        
+
+    }
+}

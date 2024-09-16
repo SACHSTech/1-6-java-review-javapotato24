04b2d0c2e32a99f42c76316a712defc86e549e64
javapotato24 <evan.wu25@ycdsbk12.ca>
2024-09-16 10:16:01 -0400

    finished Review7

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -10,13 +10,29 @@ public class Review7{
         String strSentence = sentence.readLine();
 
         int intLength = strSentence.length();
+        int intSpaces = 0;
+        int intOdd = 0;
+        int intA = 0;
         String strLetter;
         
         for(int i = 0; i < intLength; i++ ){ 
             strLetter = strSentence.substring(i , intLength);
-            if(strLetter == " "){
-                
+            if(strSentence.charAt(i) == ' '){
+                intSpaces += 1;
             }
+            if(i%2 == 0){
+                intOdd += 1;
+            }   
+            if(strSentence.charAt(i) == 'a'){
+                intA += 1;
+            }
+        }
+
+        System.out.println("There are " + intLength + " characters in the sentence.");
+        System.out.println("There are " + intSpaces + " spaces in the sentence.");
+        System.out.println("There are " + intA + " letter a in the sentence.");
+        for(int i = 0; i < intOdd; i++){
+            System.out.print("-");
         }
 
     }

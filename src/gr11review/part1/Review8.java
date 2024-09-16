d674ed995bcffc25a286ae1c9c8676eb68885de6
javapotato24 <evan.wu25@ycdsbk12.ca>
2024-09-16 10:26:29 -0400

    Finished review 8

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -1,1 +1,30 @@
 package gr11review.part1;
+
+
+import java.io.*;
+import java.util.Random;
+
+public class Review8{
+    public static void main(String[] args) throws IOException{
+        
+        Random rand = new Random();
+        int intNum1;
+        int intNum2;
+        int intNum3;
+        int intTriple = 0;
+
+        for(int i = 0; i < 1000; i++){
+            intNum1 = rand.nextInt(8);
+            intNum2 = rand.nextInt(8);
+            intNum3 = rand.nextInt(8);
+
+            if( intNum1 == intNum2 && intNum2 == intNum3){
+                intTriple += 1;
+            }
+
+            System.out.println(intNum1 + " " + intNum2 + " " + intNum3);
+        }
+        System.out.println(intTriple);
+
+    }
+}

fa860e98c7a10a772c0d3fcdf6b678cb4bf92efb
javapotato24 <evan.wu25@ycdsbk12.ca>
2024-09-16 10:39:08 -0400

    added javadocs for review 1 and 2

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -1,22 +1,40 @@
 package gr11review.part1;
 
 import java.io.*;
+/**
+ * The program Review2 class gives the user a list of 4 different jokes to pick from and describes 
+ * the theme of the joke to the user. The program then outputs a different joke based on the uesr input
+ * 
+ * This program makes use of switch cases to determin which joke to oputput basesd on the input from the user
+ * 
+ * @author Evan Wu 
+ */
 
 public class Review2{
+    /**
+     * Main method to execute the program logic.
+     * 
+     * @param args Command-line arguemnts (not used).
+     * @throws IOException If there is an error during input.
+     */
     public static void main(String[] args) throws IOException{
         BufferedReader menu = new BufferedReader(new InputStreamReader(System.in));
 
+        //initializing the variables
         int intMenu;
 
-        // System.out.print("0 - print a joke about your hair" + '\n' + "1 - print a joke about your feet" + '\n' + "2 - print a joke about your clothes" + '\n' +"3 - print a joke about your teacher" + '\n');
+        //Outputing menu option to the user 
         System.out.println("0 - print a joke about your hair");
         System.out.println("1 - print a joke about your feet");
         System.out.println("2 - print a joke about your clothes");
         System.out.println("3 - print a joke about your teacher");
         
+        //getting user input 
         System.out.print("Choose a menu option: ");
         intMenu = Integer.parseInt(menu.readLine());
 
+        //using switch case to determin user input and then outputting a joke to the user based on 
+        //option chosen
         switch(intMenu){
             case 0:
                 System.out.println("Why do bees have sticky hair? \nThey always use honeycombs. ");
